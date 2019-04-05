76234d5b6cf189a3c77ed7de6b480a50cedbf6aa
Wanda <doubleuevans@gmail.com>
2019-04-05 11:29:39 -0500

    cleaned up code

--- names/names.py
+++ names/names.py
@@ -10,6 +10,7 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+
 duplicates = [name for name in names_1 if name in names_2]
 
 # duplicates = [] 
@@ -17,8 +18,6 @@ duplicates = [name for name in names_1 if name in names_2]
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
-
-
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
