48552e2ae5c5897cb1317dbf5a75d1d1fabd7f37
Wanda <doubleuevans@gmail.com>
2019-04-05 09:54:42 -0500

    added clarifications and strategy comments

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -9,3 +9,27 @@ class RingBuffer:
 
   def get(self):
     pass
+
+""" What is a Ring Buffer?
+A ring buffer is also known as a circular buffer. 
+It is FIXED in size.
+It's a queue FIFO.
+ """
+
+""" What do we want?
+APPEND
+If our buffer is filled, we want the newest element to OVERWRITE the oldest element. 
+
+  1. [a, b, c, d], add element(e), now array is [e, b, c, d]
+  2. [e, b, c, d], add elemtent(f), now array is [e, f, c, d]
+
+So if an element array is None pop/append like a normal queue. 
+If it is full SWAP with the oldest item in the queue.
+
+GET
+return the array
+ """
+
+""" What I know:
+The empty array we start with has a size/length. The Nones are counted.
+ """
